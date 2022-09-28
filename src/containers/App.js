308fc43e6c8135cc4f44ed918a0755fe3365385c
angeliwanov <angel.iwanov@gmail.com>
2022-09-28 16:38:26 +0300

    click counter

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,12 +9,14 @@ import ErrorBoundary from "../components/ErrorBoundary";
 const App = () =>  {
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
 
     useEffect(() =>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users))
-    },[])
+        console.log(count)
+    },[count])
 
     const onSearchChange = (event) => {
         setSearchField(event.target.value);
@@ -28,6 +30,7 @@ const App = () =>  {
     <h1>Loading...</h1> : 
     <div className="tc">
         <h1 className="f2">RoboFriends</h1>
+        <button onClick={()=>setCount(count+1)}>Click me!</button>
         <Searchbox SearchChange={onSearchChange}/>
         <Scroll>
             <ErrorBoundary>
